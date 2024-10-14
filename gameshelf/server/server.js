77f46a6da83b49e8dad91aa7f2523094cc99b31e
Juliana <jsnorek340@gmail.com>
2024-10-14 14:28:39 -0700

    successful get request for fetching database reviews for game details page

--- gameshelf/server/server.js
+++ gameshelf/server/server.js
@@ -66,6 +66,22 @@ app.get('/reviews/:userId', async (req, res) => {
     }
 });
 
+//request for database reviews for game details page
+app.get('/game-reviews/:gameId', async (req, res) => {
+    const gameId = req.params.gameId;
+    try {
+        const queryText = 'SELECT review_text, rating FROM reviews WHERE game_id = $1';
+        const { rows } = await db.query(queryText, [gameId]);
+        if(rows.length === 0) {
+            return res.status(404).send({ message: 'No reviews found for this game' });
+        }
+        res.status(200).json(rows);
+    } catch (error) {
+        console.error('Error fetching game reviews', error);
+        res.status(500).send('Server error');
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Hi, server listening on ${PORT}`);
 });
