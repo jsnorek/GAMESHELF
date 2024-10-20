d15aff98ee22da32593980f66413b092f00f94c5
Juliana <jsnorek340@gmail.com>
2024-10-20 13:55:19 -0700

    adding more game details to modal

--- client/src/components/GameDetailsModal.jsx
+++ client/src/components/GameDetailsModal.jsx
@@ -9,6 +9,10 @@ function GameDetailsModal({ setGameDetailsModalVisible, gameDetails }) {
 
     console.log('these are the game details in the modal', gameDetails)
 
+    // const platformsList = gameDetails?.platforms?.map((platform, index) => (
+    //     <li key={index}>{platform.platform.name}</li>
+    // ));
+
     return (
         <div className="game-details-modal">
             <p>Game Details</p>
@@ -18,7 +22,16 @@ function GameDetailsModal({ setGameDetailsModalVisible, gameDetails }) {
                         <>
                         <p>{gameDetails.name}</p>
                         <img src={gameDetails.background_image} alt={gameDetails.name} style={{ width: '200px' }}/>
-                        {/* <p>{gameDetails.description_raw}</p> */}
+                        <p>{gameDetails.description_raw}</p>
+                        <p>Platform(s): {gameDetails.platforms.map((platform, index) => (
+                            <li key={index}>{platform.platform.name}</li>
+                        ))}</p>
+                        <p>Genre: {gameDetails.genres.map((genre, index) => (
+                            <li key={index}>{genre.name}</li>
+                        ))}</p>
+                        <p>Publisher(s): {gameDetails.publishers.map((publisher, index) => (
+                            <li key={index}>{publisher.name}</li>
+                        ))}</p>
                         </>
                     ) : (
                         <p>No game details available</p>
