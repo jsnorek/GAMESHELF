fcccc118ed47d0a417eb865b03949259f1893cd1
Juliana <jsnorek340@gmail.com>
2024-10-22 12:59:34 -0700

    modal popup for user reviews

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,12 +1,67 @@
 // Will eventually render the Profile page with list of user's information from the database
 
+import { Button } from "primereact/button";
+import { useState } from "react";
+
 function Profile({ fullLoggedInUserData }) {
+    const [userReviewsListVisible, setUserReviewsListVisible] = useState(false);
+
+    const userData = fullLoggedInUserData[0];
+
+    const handleUserReviewsVisible = () => {
+        setUserReviewsListVisible(true)
+        console.log('user review list is visible', userReviewsListVisible);
+      };
+
+    const closeUserReviewModal = () => {
+        setUserReviewsListVisible(false)
+    };
+
+    console.log('is user reviews list visible?', userReviewsListVisible);
+
+    //   {gameReviews && gameReviews.length > 0 ? (
+    //     <ul>
+    //         {gameReviews.map((review, index) => (
+    //             <li key={index}>
+    //                 <strong>Rating: </strong> {review.rating} <br />
+    //                 <strong>Review: </strong> {review.review_text}
+    //                 <p>- {review.username}</p>
+    //                 <p>{new Date(review.created_at).toLocaleDateString()}</p>
+    //             </li>
+    //         ))}
+    //     </ul>
+    //     ) : (
+    //     <p>No reviews available</p>
+    // )}
 
     return (
         <div>
-            <h1>Hello, {fullLoggedInUserData[0].name}</h1>
-            <p>Username: {fullLoggedInUserData[0].username}</p>
-            <p>Email: {fullLoggedInUserData[0].email}</p>
+            <h1>Hello, {userData.name}</h1>
+            <p>Username: {userData.username}</p>
+            <p>Email: {userData.email}</p>
+            <p>City: {userData.city}</p>
+            <Button label="My Reviews" onClick={handleUserReviewsVisible}/>
+            {userReviewsListVisible && (userData.reviews && userData.reviews.length > 0 ? (
+                <div className="user-reviews-list">
+                    <ul>
+                        {userData.reviews.map((review, index) => (
+                            <>
+                                <li key={index}>
+                                    <p>Rating: </p> {review.rating}
+                                    <p>Review: </p> {review.review_text}
+                                </li>
+                                <Button label="Back" onClick={closeUserReviewModal}/>
+                            </>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
+                <div className="user-reviews-list">
+                    <p>No reviews available</p>
+                    <Button label="Back" onClick={closeUserReviewModal}/>
+                </div>
+            )
+            )}
         </div>
     )
 }
