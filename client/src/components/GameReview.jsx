62225c8bc7c7c2e9b1eec2cc4ac4e73da42d0c8d
Juliana <jsnorek340@gmail.com>
2024-10-28 14:57:31 -0700

    added successful testing for gamereviewform

--- client/src/components/GameReview.jsx
+++ client/src/components/GameReview.jsx
@@ -3,13 +3,15 @@
 
 function GameReview({ review }) {
     return (
-        <li>
-            <strong>Rating: </strong> {review.rating} <br />
-            <strong>Review: </strong> {review.review_text}
-            {review.username && <p>{review.username}</p>}
-            {/* <p>- {review.username}</p> */}
-            <p>{new Date(review.created_at).toLocaleDateString()}</p>
-        </li>
+        <div data-testid="game-review">
+            <li>
+                <strong data-testid="review-rating">Rating: </strong> {review.rating} <br />
+                <strong>Review: </strong> {review.review_text}
+                {review.username && <p>{review.username}</p>}
+                {/* <p>- {review.username}</p> */}
+                <p>{new Date(review.created_at).toLocaleDateString()}</p>
+            </li>
+        </div>
     );
 }
 
